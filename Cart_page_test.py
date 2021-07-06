90ad1c1de1c9e3ec97c238866dca60a5fbc9509f
Gendor <ckakodybcsgo@gmail.com>
2021-07-07 00:56:47 +0300

    add delete_book_test to Cart_page_test.py

--- Cart_page_test.py
+++ Cart_page_test.py
@@ -62,5 +62,29 @@ def count_update():
         return 0
 
 
+def delete_book():
+    driver.get('http://localhost:8080/books')
+    driver.find_element_by_xpath(f'//*[@id="app"]/section/article[1]/div[2]/div/div[2]/div/a').click()
+    driver.find_element_by_xpath(f'//*[@id="app"]/section/article[2]/div[2]/div/div[2]/div/a').click()
+    driver.get('http://localhost:8080/cart')
+    count_of_books = len(driver.find_elements_by_class_name('media'))
+    driver.find_element_by_xpath('//*[@id="app"]/section/div/div[1]/article[1]/div[1]'
+                                 '/div/div/div/div[3]/button').click()
+    new_count_of_books = len(driver.find_elements_by_class_name('media'))
+    if new_count_of_books == count_of_books:
+        return 0, 'книга не удалилась'
+    if count_of_books - new_count_of_books > 1:
+        return 0, 'удалилось больше одной книги'
+    else:
+        return 1
 
 
+print("1 - система прошла тест, 0 - система не прошла тест")
+print("Тестирование системы...")
+first_test_result = check_price()
+print(f"Цена книг соответсвует стоимости заказа          - {first_test_result}")
+second_test_result = count_update()
+print(f"Количество книг в корзине корректно обновляется  - {second_test_result}")
+third_test_result = delete_book()
+print(f"Книга корректно удаляется из корзины             - "
+      f"{1 if third_test_result == 1 else f'0, {third_test_result[1]}'}")
